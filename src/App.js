85cb95778bfe30db238b5013d87218f19c56ec71
Advitiya <advitiya993@gmail.com>
2024-08-17 00:58:32 +0530

    added login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Herosec from './components/Herosec';
 import Nav from './components/Nav';
-
+import LoginPage from './components/Login';
 import { Component } from 'react';
 
 function App() {
   return (
     <div>
-      <Nav/>
-      <Herosec/>
+    <Nav/>
+    {/* <Herosec/> */}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        
+        <Route path="/" element={<Herosec />} />
+        
+      </Routes>
+    </Router>
+   {/* <Herosec/> */}
+    {/* //   <LoginPage/> */}
     </div>
+    
   );
 }
 
