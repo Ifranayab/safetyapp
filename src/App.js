0a1e35abc02a3ef123aab43a8dd3e56456dfa3ef
ifranayab <ifra07nayab@gmail.com>
2024-11-20 16:28:22 +0530

    merged the conflicts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 // import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Herosec from './components/Herosec';
 import Nav from './components/Nav';
 import LoginPage from './components/Login';
 import Aboutus from './components/Aboutus';
 import Contact from './components/Contact';
 import Cards from './components/CardsSec';
+import Footer from './components/Footer';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div>
-      <Router>
-        <Nav />
-        <Routes>
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/" element={<> <Herosec /> <Cards/> </>} />
-        </Routes>
-      </Router>
+    <div className='bg-orange-100 overflow-x-hidden'>
+      {/* Render Nav only if not on the login page */}
+      {location.pathname !== "/login" && <Nav />}
+
+      <Routes>
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/" element={<><Herosec /><Cards /></>} />
+      </Routes>
+
+      {/* About Us and Contact Us sections always rendered */}
       <div id="about-us">
         <Aboutus />
       </div>
       <div id="contact-us">
         <Contact />
       </div>
+
+      {/* Render Footer only on the home page */}
+      {location.pathname === "/" && <Footer />}
     </div>
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWrapper;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,6 @@
-// import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
-// import logo from '../images/logo.png';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faEnvelope,faBars } from '@fortawesome/free-solid-svg-icons'
-
-// const Nav =() =>{
-//   return (
-//     <div className='h-20 bg-custom-gradient fixed w-[100vw] z-10'>
-//       <div className='flex  justify-start items-center py-6 px-6'>
-//         <img  className='h-10 hover:border-2 hover:border-white hover:rounded-sm' src={logo} alt="logo" />
-//           <Link className='mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm' to="/">Home</Link>
-//           <Link className='mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm'>About Us</Link>
-//           <Link className='mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm'>Contact Us</Link>
-//           <div className='ml-auto mr-10 bg-pink-600 h-18 py-1 px-4 rounded-md cursor-pointer hover:border-2 hover:border-white'>
-//           <Link className='text-xl font-extrabold text-white' to="/login">Login</Link>
-//           </div>
-          
-//       </div>
-//     </div>
-//   )
-// }
-// export default Nav
-import React from 'react';
+// import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
   const scrollToSection = (sectionId) => {
@@ -36,17 +11,16 @@ const Nav = () => {
   };
 
   return (
-    <div className='h-20 bg-custom-gradient fixed w-[100vw]'>
-      <div className='flex  justify-start items-center py-6 px-6'>
-        <img  className='h-10 hover:border-2 hover:border-white hover:rounded-sm' src={logo} alt="logo" />
-          <Link className='mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm' to="/">Home</Link>
-          <Link className='mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm'>About Us</Link>
-          <Link className='mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm'>Contact Us</Link>
-          <Link className='mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm'>Review</Link>
-          <div className='ml-auto mr-10 bg-pink-600 h-18 py-1 px-4 rounded-md cursor-pointer hover:border-2 hover:border-white'>
-          <Link className='text-xl font-extrabold text-white' to="/login">Login</Link>
-          </div>
-          
+    <div className="h-20 bg-custom-gradient fixed w-[100vw] z-10">
+      <div className="flex justify-start items-center py-6 px-6">
+        <img className="h-10 hover:border-2 hover:border-white hover:rounded-sm cursor-pointer" src={logo} alt="logo" onClick={() => scrollToSection('home')} />
+        <a onClick={() => scrollToSection('home')} className="mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm">Home</a>
+        <a onClick={() => scrollToSection('about-us')} className="mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm">About Us</a>
+        <a onClick={() => scrollToSection('contact-us')} className="mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm">Contact Us</a>
+        <a onClick={() => scrollToSection('review')} className="mx-5 text-xl font-bold text-white cursor-pointer hover:border-2 hover:border-white hover:rounded-sm">Review</a>
+        <div className="ml-auto mr-10 bg-pink-600 h-18 py-1 px-4 rounded-md cursor-pointer hover:border-2 hover:border-white">
+          <Link className="text-xl font-extrabold text-white" to="/login">Login</Link>
+        </div>
       </div>
     </div>
   );
